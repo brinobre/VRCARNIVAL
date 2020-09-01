12596f909610a7b531fe3b25899bba88e8e9aef7
Armin Kemura <52348946+brinobre@users.noreply.github.com>
2020-09-01 17:40:26 +0200

    added xr controllers, teleportation and turning

---
+++ VR CARNIVAL/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportController : MonoBehaviour
+{
+    public XRController rightTeleportRay;
+    public XRController leftTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+
+    void Start()
+    {
+        rightTeleportRay.gameObject.SetActive(false);
+        leftTeleportRay.gameObject.SetActive(false);
+    }
+
+    bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated);
+        return isActivated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay) && !leftTeleportRay.gameObject.activeInHierarchy);
+        leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay) && !rightTeleportRay.gameObject.activeInHierarchy);
+    }
+}
