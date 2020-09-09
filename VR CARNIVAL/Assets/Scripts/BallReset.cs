e7f8f32fdc13b4a5360c6a658b713937334bbb86
zibiax <33799767+zibiax@users.noreply.github.com>
2020-09-09 17:08:36 +0200

    Added bowling game

---
+++ VR CARNIVAL/Assets/Scripts/BallReset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour
+{
+    private Vector3 startPosition;
+    
+    void Start()
+   {
+        startPosition = transform.position;
+   }
+    // Remembering where the ball started
+   void Update()
+   {
+   if (transform.position.y < -5f)
+        {
+            transform.position = startPosition;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+   // Reset of the ball is triggered when the ball is falling with the force of 5
+   }
+}
---
+++ VR CARNIVAL/Assets/Scripts/BallSizeChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSizeChange : MonoBehaviour
+{
+    public void GrowBall()
+    {
+        transform.localScale *= 1.1f;
+    }
+    public void ShrinkBall()
+    {
+        transform.localScale *= 0.1f;
+    }
+}
